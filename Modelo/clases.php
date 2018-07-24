2f43c56db5551fc849c6c9ed531d20bfbdd92293
} <>
2018-07-24 11:00:13 -0500

    funciones

--- Modelo/clases.php
+++ Modelo/clases.php
@@ -110,59 +110,7 @@
 		return '';
 	}
 
-	public function check($source, $items = array()){//en esta funcion esta mirando errores
-			foreach ($items as $item => $rules) {
-				foreach ($rules as $rule => $rule_value) {
-								
-					$value = trim($source[$item]);
-					$item = escape($item);
-					
-					if ($rule === 'required' && empty($value)) {
-						$this->addError("{$item} is required");
-					}else if(!empty($value)){
-						switch ($rule) {
-							case 'min':
-								if (strlen($value) < $rule_value) {
-									$this->addError("{$item} debe ser de un minimo de {$rule_value} caracteres.");
-								}
-							break;
-							case 'max':
-								if (strlen($value) > $rule_value) {
-									$this->addError("{$item} debe ser de un maximo de {$rule_value} caracteres.");
-								}
-							break;
-							case 'matches':
-								if ($value != $source[$rule_value]) {
-									$this->addError("{$rule_value} debe coincidir con {$item}");
-								}
-							break;
-							case 'unique':
-								$check = $this->_db->get($rule_value, array($item, '=', $value));
-								if ($check->count()) {
-									$this->addError("{$item} ya existe.");
-								}
-							break;
-							default:
-								# code...
-							break;
-						}
-					}
-				}
-			}
-			if (empty($this->_errors)) {
-				$this->_passed = true;
-			}
-			return $this;
-		}
-		private function addError($error){
-			$this->_errors[] = $error;
-		}
-		public function errors(){
-			return $this->_errors;
-		}
-		public function passed(){
-			return $this->_passed;
-		}
+	
 	}
 
 
