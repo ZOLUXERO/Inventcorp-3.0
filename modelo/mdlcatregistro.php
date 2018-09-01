147fd48308cbe2304bbc35f4e29201e3d8d81b18
ZOLUXERO <34288988+ZOLUXERO@users.noreply.github.com>
2018-09-01 16:09:17 -0500

    seguridad

--- modelo/mdlcatregistro.php
+++ modelo/mdlcatregistro.php
@@ -5,10 +5,10 @@ include_once("conexion.php");
 class Categoriareg extends Conexion
 {
 	
-	public function registrocat($cod, $nom, $des)
+	public function registrocat($nom, $des)
     {
         //$q = "select * from usuario where usuario='$usu' and contrasena='$cont'";
-        $q = "insert into categorias(id_categoria, nombre_categoria, desc_categoria, estado_categoria) values('$cod','$nom','$des',1)";
+        $q = "insert into categorias(nombre_categoria, desc_categoria, estado_categoria) values('$nom','$des',1)";
         $consulta = $this->con->query($q) or die ('Error!' . $this->con->error);
         
     }
