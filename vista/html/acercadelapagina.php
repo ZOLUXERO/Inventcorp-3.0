146ef57ef1fb30710618e11e93bb60362ec18a09
zoluxero <zoluxero@gmail.com>
2019-08-14 00:02:55 -0500

    cambios para port 2

--- vista/html/acercadelapagina.php
+++ vista/html/acercadelapagina.php
@@ -35,8 +35,8 @@ if(isset($_REQUEST['visitante'])){
 
         <div class="row featurette">
         <div class="col-md-7">
-          <h2 class="featurette-heading">Acerca de nosotros. <span class="text-muted">¿Quiénes somos?</span></h2>
-          <p class="lead">Somos un grupo de estudiantes del Sena que está cursando análisis y desarrollo de sistemas de información. Trabajamos continuamente en esta página para darle vida a este proyecto que tiene como objetivo brindar un software orientado a la web que sea seguro y eficiente.</p>
+          <h2 class="featurette-heading">Acerca de la pagina. <span class="text-muted">¿funcionalidades?</span></h2>
+          <p class="lead">Si presiona el boton de PRUEBA entrara al sistema sin restriccion alguna esto con el fin de que pueda ver el sistema en funcionamiento, si desea registrarse tambien puede hacerlo pero entrara como visitante hasta que un administrador le de los respectivos permisos. Debido a que el proyecto ya se termino es poco probable que un administrador entre a darle permisos por esto el boton de prueba.</p>
         </div>
         <div class="col-md-5">
            <img class="featurette-image img-responsive center-block" src="../imagenes/c5.png" width="250" alt="Generic placeholder image">
@@ -50,8 +50,8 @@ if(isset($_REQUEST['visitante'])){
          <img class="featurette-image img-responsive center-block" src="../imagenes/c5.png" width="250" alt="Generic placeholder image">
         </div>
         <div class="col-md-7">
-          <h2 class="featurette-heading">Acerca de la pagina. <span class="text-muted">¿funcionalidades?</span></h2>
-          <p class="lead">/Espacio para escribir funcionalidades/ /Espacio para escribir funcionalidades/ /Espacio para escribir funcionalidades/ /Espacio para escribir funcionalidades/ /Espacio para escribir funcionalidades/ /Espacio para escribir funcionalidades/.</p>
+          <h2 class="featurette-heading">Tour Rapido <span class="text-muted"> Graficas!</span></h2>
+          <p class="lead">Si desea ver de que trata esta pagina de forma rapida lo invito a que presione Prueba, en el menu que le aparecera a la izquierda presione productos y de seguimiento en el producto que desee.</p>
         </div>
 
       </div>
@@ -60,8 +60,8 @@ if(isset($_REQUEST['visitante'])){
 
       <div class="row featurette">
         <div class="col-md-7 ">
-          <h2 class="featurette-heading">Automatízate con Inventcorp <span class="text-muted">Software de inventario.</span></h2>
-          <p class="lead">Maneje su inventario de una manera mucho mas sencilla, deje de complicarse y regístrese gratis.</p>
+          <h2 class="featurette-heading">Acerca de nosotros. <span class="text-muted">¿Quiénes somos?</span></h2>
+          <p class="lead">Somos un grupo de estudiantes del Sena que está cursando análisis y desarrollo de sistemas de información. Trabajamos continuamente en esta página para darle vida a este proyecto que tiene como objetivo brindar un software orientado a la web que sea seguro y eficiente. !</p>
         </div>
         <div class="col-md-5 ">
          <img class="featurette-image img-responsive center-block" src="../imagenes/c5.png" width="250" alt="Generic placeholder image">
@@ -77,8 +77,8 @@ if(isset($_REQUEST['visitante'])){
           <img class="featurette-image img-responsive center-block" src="../imagenes/c5.png" width="250" alt="Generic placeholder image">
         </div>        
         <div class="col-md-7">
-          <h2 class="featurette-heading">Mas espacio para escribir <span class="text-muted">Mas para escribir.</span></h2>
-          <p class="lead">/Espacio para escribir /Espacio para escribir /Espacio para escribir /Espacio para escribir /Espacio para escribir /Espacio para escribir /Espacio para escribir.</p>
+          <h2 class="featurette-heading">Automatízate con Inventcorp <span class="text-muted">Software de inventario.</span></h2>
+          <p class="lead">Maneje su inventario de una manera mucho mas sencilla, deje de complicarse y regístrese gratis.</p>
         </div>
 
       </div>
